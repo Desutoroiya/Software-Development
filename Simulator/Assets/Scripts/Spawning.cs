59496f53ff927aab302f0994b9e0088a64b8d1e0
Ivar de Lange <ivardelange2003@hotmail.com>
2015-01-06 14:35:07 +0100

    Update 12
    Train done, ww2 airplane fixed

--- Simulator/Assets/Scripts/Spawning.cs
+++ Simulator/Assets/Scripts/Spawning.cs
@@ -13,7 +13,31 @@ public class Spawning : MonoBehaviour {
 	private float counter;
 	private GameObject[] wayPoints;
 	private int currentPoint;
+
+	public bool messageTreinDelete = false;
+	public bool selfSpawner = true;
+	private Client net;
+	public OutMessageBehaviour treinMessage;
+	public bool isTrein = false;
+	public Spoorboom sp;
 	// Use this for initialization
+
+	void sendMessage(int count)
+	{
+		if(isTrein)
+		{
+			net = GameObject.FindObjectOfType(typeof(Client)) as Client;
+			OutMessage om = new OutMessage ();
+			om.num = treinMessage.num;
+			om.inbound = treinMessage.inBound;
+			om.outbound = treinMessage.outBound;
+			om.vehicle = treinMessage.vehicle;
+			treinMessage.count = count;
+			om.count = count;
+			net.sendMessage (om.toMessage ());
+		}
+	}
+
 	void Start () {
 		spawnMeInst = (GameObject)Instantiate(spawnMe);
 		spawnMeInst.SetActive(false);
@@ -38,20 +62,41 @@ public class Spawning : MonoBehaviour {
 	
 	// Update is called once per frame
 	void Update () {
-		counter += Time.deltaTime;
-		if(counter > this.SpawnEvery){
-			if(alive < maxAlive){
-				GameObject spawnee = (GameObject)Instantiate(spawnMeInst);
-				spawnee.SetActive(true);
-				NavController nav = spawnee.GetComponent<NavController>();
-				nav.setWaypoints(wayPoints);
-				nav.spawner = this;
-				counter = 0.0f;
-				alive++;
+		if(selfSpawner)
+		{
+			if(maxAlive >= 1)
+			{
+				counter += Time.deltaTime;
+				if(counter > this.SpawnEvery)
+				{
+					if(alive < maxAlive)
+					{
+						spawner();
+						counter = 0.0f;
+						alive++;
+					}
+				}
 			}
 		}
 	}
-	
+
+
+	public void spawner()
+	{
+		GameObject spawnee = (GameObject)Instantiate(spawnMeInst);
+		if(isTrein)
+		{
+			TrainController tc = (TrainController)spawnee.GetComponent(typeof(TrainController));
+			tc.sp = this.sp;
+			sendMessage(1);
+		}
+		spawnee.SetActive(true);
+		NavController nav = spawnee.GetComponent<NavController>();
+		nav.setWaypoints(wayPoints);
+		nav.spawner = this;
+	}
+
+
 	public void carDestroyed(){
 		alive--;
 	}
